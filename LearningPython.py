93808ce55ee4c3a7f27226313d1654d82018d68a
AnjaniChitlangia <chitlangiaanjani@gmail.com>
2026-07-26 19:53:00 +0530

    rules,keywords,operators done

--- LearningPython.py
+++ LearningPython.py
@@ -1,1 +1,39 @@
-print("Hello, World!")
+#print("Anjani is my name","I am 19 years old")
+#print("Hello, World!")
+"""print(25+23)
+name="Anjani"
+print(name)
+age=19
+print("My age is ",age)
+print(type(name))"""
+#TO check the type of variable
+"""a=None
+old=False
+print(type(a))
+print(type(old))"""
+#arithmetic operations
+"""a=5
+b=6
+sum=a+b
+diff=a-b
+print(sum,diff,a*b,a/b,a%b)"""
+#relational operators
+"""a=50
+b=20
+print(a==b, a>=b)"""
+#assignment operators
+"""num=10
+num2=0
+num+=10 #num=num+10 means this 
+num2*=5 
+print(num , num2)"""
+#LogicalOperators
+"""print(not True)
+a=50
+b=20
+val1 = True
+val2 = False
+print("and operator",val1 and val2)
+print("or operator",val1 or val2)
+print(a==b or a>=b)"""
+
