ac03d68cff038cbc3fd2a6fd7c64e0caae3f702b
AnjaniChitlangia <chitlangiaanjani@gmail.com>
2026-07-28 12:52:02 +0530

    strings , indexing , slicing

---
+++ Lecture_2.py
@@ -0,0 +1,17 @@
+"""str="This is a string , We are creating it in python"
+str1='This is also a string'
+str2=""This is a multi line string . \n tHIS is a multi line string""
+print(str2)"""
+#concatenation of strings
+"""str1="Hello"
+str2="World"   
+print(str1+str2)
+print(len(str1))"""
+#indexing of strings
+"""print(str1[3] + str2[4])"""
+#slicing of strings
+str="Hello World"
+print(str[0:5])
+print (str[3:len(str)])
+str1="Apple"
+print(str1[-3:-1])
