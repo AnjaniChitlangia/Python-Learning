bd2054864d1118648613ed2383791c3dfada647b
AnjaniChitlangia <chitlangiaanjani@gmail.com>
2026-08-16 00:29:59 +0530

    functions

---
+++ Lecture_6.py
@@ -0,0 +1,21 @@
+def calc_sum(a,b):
+    s=a+b
+    return s
+print(calc_sum(5,6))
+print(calc_sum(10,20))
+def print_hello():
+    print("Hello")
+print_hello()
+
+#average 
+def calc_avg(a,b,c):
+    avg=(a+b+c)/3
+    return avg
+print(calc_avg(10,20,30))
+
+print("Anjani Chitlangia" , end="")
+print("I am learning Python from Apnacollege" , end="\n")
+print("Thank you shraddha kapra mam")
+#output will be 
+#Anjani Chitlangia I am learning Python from Apnacollege 
+#Thank you shraddha kapra mam
